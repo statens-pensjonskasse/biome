9a410e83650f35fa8f46f77973457751d2b6f716
Bjørn Gilstad <bjorn.gilstad@spk.no>
2022-01-21 14:20:28 +0100

    Oppdater deps, legg til no-depricated og import order

--- typescript.js
+++ typescript.js
@@ -7,7 +7,24 @@ module.exports = {
     '@typescript-eslint/ban-ts-comment': 'off', // lagt til etter oppgradering til eslint 7.
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'import/no-deprecated': 'warn',
   },
-  plugins: ['@typescript-eslint'],
-  parser: '@typescript-eslint/parser'
+  plugins: ['@typescript-eslint', 'import'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: ".",
+    sourceType: "module",
+    project: "./tsconfig.json"
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"]
+    },
+    "import/resolver": {
+      typescript: {
+        "alwaysTryTypes": true,
+        "paths": "./tsconfig.json"
+      }
+    }
+  }
 }
