3c749a849911015c01b01e8b82556351b76c56c9
Robin Gulbrandsen <robin.gulbrandsen@spk.no>
2024-12-03 13:26:32 +0100

    rapportgenerering

--- report_run.sh
+++ report_run.sh
@@ -1,6 +1,6 @@
 echo Generer rapport for Biome
 
-npx biome check --reporter=github --config-path ./biome-all.json > tmp
+npx biome check --reporter=github --config-path=./biome-all.json > tmp
 
 ./report_generate.sh tmp report.md
 
