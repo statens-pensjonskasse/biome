f2da678c45bbde64a7da1da037978f7581b641a2
Robin Gulbrandsen <robin.gulbrandsen@spk.no>
2024-12-03 13:39:06 +0100

    fikset linker til dokumentasjon

--- report_generate.sh
+++ report_generate.sh
@@ -15,6 +15,12 @@ count_occurrences() {
     grep -c "$pattern" "$file"
 }
 
+convert_to_kebab_case() {
+    local str="$1"
+    # First replace camelCase with dash separation, then convert to lowercase
+    echo "$str" | sed -E 's/([a-z0-9])([A-Z])/\1-\2/g' | tr '[:upper:]' '[:lower:]'
+}
+
 # Count total diagnostics
 total_errors=$(grep "::error" "$input_file" |
     sed 's/.*title=\([^,]*\),.*/\1/' |
@@ -54,9 +60,10 @@ sort | uniq -c |
 while read -r count rule; do
     category=$(echo "$rule" | cut -d'/' -f1)
     rulename=$(echo "$rule" | cut -d'/' -f2-)
+    rulenamekebab=$(convert_to_kebab_case  rulename)
     # Skip format, parse and organizeImports categories
 	if [ "$category" != "format" ] && [ "$category" != "organizeImports" ] && [ "$category" != "parse" ]; then
-		echo "| $category | [\`$rulename\`](https://biomejs.dev/linter/rules/${rulename}) | $count |" >> "$output_file"
+		echo "| $category | [\`$rulename\`](https://biomejs.dev/linter/rules/${rulenamekebab}) | $count |" >> "$output_file"
 	fi
 done
 
