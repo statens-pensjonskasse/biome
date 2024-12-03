35b661402436436685e853738842780492f32419
Robin Gulbrandsen <robin.gulbrandsen@spk.no>
2024-12-03 13:33:03 +0100

    skrevet om path for at det skal fungere i prosjektene

--- report_run.sh
+++ report_run.sh
@@ -1,7 +1,7 @@
 echo Generer rapport for Biome
 
-npx biome check --reporter=github --config-path=./biome-all.json > tmp
+npx biome check --reporter=github --config-path=./node_modules/@spk/biome/biome-all.json > tmp
 
-./report_generate.sh tmp report.md
+./node_modules/@spk/biome/report_generate.sh tmp report.md
 
 rm tmp
