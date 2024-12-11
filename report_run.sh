2c57c551aa0e20533e685a7c831405e8e6f8f7f5
Robin Gulbrandsen <robin.gulbrandsen@spk.no>
2024-12-11 14:27:28 +0100

    bruker felles config ved rapport så rapporten avdekker lokalt rusk

--- report_run.sh
+++ report_run.sh
@@ -1,6 +1,6 @@
 echo Generer rapport for Biome
 
-npx biome check --reporter=github --config-path=./node_modules/@spk/biome/biome-all.json > tmp
+npx biome check --reporter=github --config-path=./node_modules/@spk/biome/biome.json > tmp
 
 ./node_modules/@spk/biome/report_generate.sh tmp report-$(date +%Y-%m-%d).md
 
